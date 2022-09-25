f4e53b4b2d21f13cbd665ca3072c90c71f8556df
raisul2 <raisul58208@gmail.com>
2022-09-25 18:59:33 +0600

    iniit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const notes = require('./data/notes');
+const dotenv = require('dotenv')
+const app = express();
+dotenv.config({path:"backend/.env"})
+app.get('/',(req ,res) =>{
+             res.send("api is running")    
+})
+
+app.get('/api/notes', (req, res) => {
+   res.json(notes)
+})
+app.get('/api/notes/:id', (req, res) => {
+    const note = notes.find((n)=>n._id === req.params.id);
+    
+    res.json(note)
+    
+
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`server stated on port ${PORT}`))
+ 
